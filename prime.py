fe477487bb918fae659aa9798199ea6a7e34daf0
Luo0105 <207834210+Luo0105@users.noreply.github.com>
2025-05-26 17:38:44 +0900

    Add files via upload

--- prime.py
+++ prime.py
@@ -1,8 +1,18 @@
 # prime number
+import math
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for a in range (2, math.isqrt(num)+1):
+        if num % a == 0:
+            break
+    else:
+        return True
+
+
 n = int(input("Enter a number: "))
-for a in range (2, n):
-    if n % a == 0:
-        print(n, 'equals', a, '*', n//a)
-        break
+if is_prime(n):
+    print(n, "is a prime number")
 else:
-    print(n, 'is a prime number')
+    print(n, "is not a prime number")
