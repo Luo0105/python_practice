15f52f84fd87f2825ecab90752d4dd6a02337888
Luo0105 <207834210+Luo0105@users.noreply.github.com>
2025-05-27 12:18:30 +0900

    Add files via upload

--- word_frecuncy.py
+++ word_frecuncy.py
@@ -4,7 +4,7 @@ from collections import Counter
 def count_word_frequency(sentence):
     cleaned_sentence = re.sub(r'[^\w\s]', '', sentence).lower()
     words = cleaned_sentence.split()
-    return Counter(words)
+    return Counter(words) # python 3.7+ provides Counter in an ordered manner, but not storing
 
 def analyze_sentence_frequency():
     user_sentence = input("请输入一个句子：")
@@ -16,4 +16,6 @@ def analyze_sentence_frequency():
         print(f"{word}: {frequency}")
 
 if __name__ == "__main__":
+    test = "Hello, world! Hello, everyone. This is a test sentence. This is only a test."
+    print(count_word_frequency(test))
     analyze_sentence_frequency()
